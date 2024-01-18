c55f108ca136517e9f3ca30d1a6542c3bfd40c4c
Илья Павлов <p4vlov.ilya01@gmail.com>
2024-01-18 16:57:42 +0800

    django

---
+++ blogicum/pages/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def about(request):
+    template = 'pages/about.html'
+    return render(request, template)
+
+def rules(request):
+    template = 'pages/rules.html'
+    return render(request, template)
